16b41097226507924112b43a00a1e1cb4294e180
mfuntowicz <t-mofun@microsoft.com>
2017-04-30 11:19:19 +0100

    Reduce memory footprint for python2 by using range iterator (like xrange)

--- ai_challenge/pig_chase/pig_chase_baseline.py
+++ ai_challenge/pig_chase/pig_chase_baseline.py
@@ -21,6 +21,8 @@ import sys
 
 from argparse import ArgumentParser
 from datetime import datetime
+
+import six
 from os import path
 from threading import Thread, active_count
 from time import sleep
@@ -97,7 +99,7 @@ def agent_factory(name, role, baseline_agent, clients, max_epochs,
         viz_rewards = []
 
         max_training_steps = EPOCH_SIZE * max_epochs
-        for step in range(1, max_training_steps+1):
+        for step in six.moves.range(1, max_training_steps+1):
 
             # check if env needs reset
             if env.done:
